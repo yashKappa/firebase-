bd5cb7ade1a124964d80a01e32292d0ba5579d1e
yashKappa <130564276+yashKappa@users.noreply.github.com>
2024-01-23 21:55:10 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAQw49gXmHXaf6NL_sEdwE975aMZYPMhwA",
+    authDomain: "kappa2003-bf849.firebaseapp.com",
+    databaseURL: "https://kappa2003-bf849-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "kappa2003-bf849",
+    storageBucket: "kappa2003-bf849.appspot.com",
+    messagingSenderId: "637437869948",
+    appId: "1:637437869948:web:2363e598c75e8dc2d32ef9",
+    measurementId: "G-D2Q4KP732E"
+  };
